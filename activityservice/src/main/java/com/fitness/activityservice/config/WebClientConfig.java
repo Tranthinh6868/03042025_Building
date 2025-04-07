d546a441433261bc2630e9e98227150a17b3588a
TranThinh <Bigfoot999>
2025-04-07 22:39:09 +0700

    Integrate RabbitMq feature p4

--- activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
@@ -1,5 +1,8 @@
 package com.fitness.activityservice.config;
 
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
